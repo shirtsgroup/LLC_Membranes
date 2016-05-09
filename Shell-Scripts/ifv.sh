73f5c0bf0e72f05e1d5308e6036fbff810f0f003
Benjamin Coscia <beco4952@login03.rc.colorado.edu>
2016-05-09 15:58:46 -0600

    Made Some scripts more general

--- Shell-Scripts/ifv.sh
+++ Shell-Scripts/ifv.sh
@@ -2,7 +2,12 @@
 # Script to copy over all necessary files for simulation of system in a vacuum
 # Edit files copied in as necessary
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 for file in wiggle.mdp NaPore.top em.mdp; do
-	cp /home/bcoscia/Documents/Gromacs/Sim_Scripts/Files/$file $PWD
+	cp $DIR/Files/$file $PWD
 
 done
+
+echo 'hello'
+
