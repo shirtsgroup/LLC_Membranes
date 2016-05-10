59285ddd6f8796bba698236bbff27a4e24fc6d87
bencoscia <benjamin.coscia@colorado.edu>
2016-05-10 14:01:38 -0600

    Adding Flags

--- Shell-Scripts/PJV.sh
+++ Shell-Scripts/PJV.sh
@@ -6,7 +6,7 @@
 # Copy in necessary files
 ifv.sh
 
-# Define Variables
+# Define Variables with Default values
 
 # Choose which monomer to build with
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"  # Directory where this script is located
@@ -55,6 +55,42 @@ REF_P=1  # Reference Pressure, bar
 COMPRESSIBILITY=4.5e-5  # Isothermal compressibility, bar^-1
 PBC='xyz'
 
+# Reference for flags associated with each variable
+
+# -m  :   MONOMER='monomer4.pdb'  # Structure file to be used
+# -i  :   INPUT_FILE=$DIR/../Structure-Files/$MONOMER  # Full path to file where monomer structure is located
+# -I  :   INTEGRATOR_EM='steep'  # Integrator for energy minimization
+# -s  :   NSTEPS_EM=50000  # Maximum number of steps to take for energy minimization
+# -c  :   CUTOFF_EM='verlet'  # Cut-off Scheme
+# -t  :   NSTLIST=10  # Neighborlist - changed automatically by gromacs unless it is set equal to 1
+# -n  :   NO_MONOMERS ... Number of monomers in 1 layer
+# -r  :   RADIUS ... Initial pore radius, angstroms
+# -p  :   PORE2PORE=40  # Pore-to-Pore distance, angstroms
+# -P  :   NOPORES=4  # Number of pores to be built
+# -w  :   DBWL=10  # Distance Between Layers
+# -l  :   LAYERS=20    # Number of layers wanted in the structure
+# -x  :   XVECT=8.0  # Box vector in the x direction
+# -y  :   YVECT=8.0  # Box vector in the y direction (not this will be multiplied by sin(120) for a monoclinic cell of 120 degrees
+#     :   INCREMENT=0.1  # Increment to increase the box vector by if there is a LINCS error
+# -T  :   SIM_TITLE='Equilibration in Vacuum'  # Title of simulation
+#CUTOFF_MD='verlet'  # Cut-off scheme for simulation
+#INTEGRATOR_MD='md'  # Integrator type for simulation
+#STEP=0.002  # Time step (ps)
+#SIM_LENGTH=1  # nanoseconds
+#NSTEPS_MD=$SIM_LENGTH*1000/$STEP  # Number of steps to be taken during simulation to simulate the desired length of time
+#FRAMES=50  # Number of frames in trajectory
+#NSTXOUT=$NSTEPS/$FRAMES  # Information output to trajectory
+#NSTVOUT=$NSTEPS/$FRAMES
+#NSTFOUT=$NSTEPS/$FRAMES
+#NSTENERGY=$NSTEPS/$FRAMES
+#TCOUPL='v-rescale'
+#REF_T=300  # Reference Temperature, K
+#PCOUPL='berendsen'
+#PCOUPLTYPE='semiisotropic'
+#REF_P=1  # Reference Pressure, bar
+#COMPRESSIBILITY=4.5e-5  # Isothermal compressibility, bar^-1
+#PBC='xyz'
+
 # Edit input files:
 
 # Energy Minimization
@@ -81,7 +117,7 @@ sed -i -e "s/REF_P/${REF_P}/g" wiggle.mdp
 sed -i -e "s/COMPRESSIBILITY/${COMPRESSIBILITY}/g" wiggle.mdp
 sed -i -e "s/PBC/${PBC}/g" wiggle.mdp
 
-# Topology
+# Edit Topology
 sed -i -e "s/MOL_LLC/${MOL_LLC}/g" NaPore.top
 sed -i -e "s/MOL_NA/${MOL_NA}/g" NaPore.top
 
