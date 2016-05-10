7bdf7b93fcb247a814991794eb77c0d42c769855
bencoscia <benjamin.coscia@colorado.edu>
2016-05-10 12:40:36 -0600

    .mdp files now customizable

--- Shell-Scripts/PJV.sh
+++ Shell-Scripts/PJV.sh
@@ -14,7 +14,7 @@ MONOMER='monomer4.pdb'  # Structure file to be used
 INPUT_FILE=$DIR/../Structure-Files/$MONOMER  # Full path to file where monomer structure is located
 
 # Energy minimization parameters:
-INTEGRATOR='steep'  # Integrator for energy minimization
+INTEGRATOR_EM='steep'  # Integrator for energy minimization
 NSTEPS_EM=50000  # Maximum number of steps to take for energy minimization
 CUTOFF_EM='verlet'  # Cut-off Scheme
 NSTLIST=10  # Neighborlist - changed automatically by gromacs unless it is set equal to 1
@@ -33,12 +33,51 @@ XVECT=8.0  # Box vector in the x direction
 YVECT=8.0  # Box vector in the y direction (not this will be multiplied by sin(120) for a monoclinic cell of 120 degrees
 INCREMENT=0.1  # Increment to increase the box vector by if there is a LINCS error
 
-sed -i -e "s/INTEGRATOR/${INTEGRATOR}/g" em.mdp
+# Simulation Parameters
+SIM_TITLE='Equilibration in Vacuum'  # Title of simulation
+CUTOFF_MD='verlet'  # Cut-off scheme for simulation
+INTEGRATOR_MD='md'  # Integrator type for simulation
+STEP=0.002  # Time step (ps)
+SIM_LENGTH=1  # nanoseconds
+NSTEPS_MD=$SIM_LENGTH*1000/$STEP  # Number of steps to be taken during simulation to simulate the desired length of time
+FRAMES=50  # Number of frames in trajectory
+NSTXOUT=$NSTEPS/$FRAMES  # Information output to trajectory
+NSTVOUT=$NSTEPS/$FRAMES
+NSTFOUT=$NSTEPS/$FRAMES
+NSTENERGY=$NSTEPS/$FRAMES
+TCOUPL='v-rescale'
+REF_T=300  # Reference Temperature, K
+PCOUPL='berendsen'
+PCOUPLTYPE='semiisotropic'
+REF_P=1  # Reference Pressure, bar
+COMPRESSIBILITY=4.5e-5  # Isothermal compressibility, bar^-1
+PBC='xyz'
+
+# Edit input files:
+
+# Energy Minimization
+sed -i -e "s/INTEGRATOR/${INTEGRATOR_EM}/g" em.mdp
 sed -i -e "s/NSTEPS_EM/${NSTEPS_EM}/g" em.mdp
 sed -i -e "s/CUTOFF_EM/${CUTOFF_EM}/g" em.mdp
 sed -i -e "s/NSTLIST_EM/${NSTLIST}/g" em.mdp
 
-
+# Wiggle.mdp
+sed -i -e "s/SIM_TITLE/${SIM_TITLE}/g" wiggle.mdp
+sed -i -e "s/CUTOFF_MD/${CUTOFF_MD}/g" wiggle.mdp
+sed -i -e "s/INTEGRATOR_MD/${INTEGRATOR_MD}/g" wiggle.mdp
+sed -i -e "s/STEP/${STEP}/g" wiggle.mdp
+sed -i -e "s/NSTEPS_MD/${NSTEPS_MD}/g" wiggle.mdp
+sed -i -e "s/NSTXOUT/${NSTXOUT}/g" wiggle.mdp
+sed -i -e "s/NSTVOUT/${NSTVOUT}/g" wiggle.mdp
+sed -i -e "s/NSTFOUT/${NSTFOUT}/g" wiggle.mdp
+sed -i -e "s/NSTENERGY/${NSTENERGY}/g" wiggle.mdp
+sed -i -e "s/TCOUPL/${TCOUPL}/g" wiggle.mdp
+sed -i -e "s/REF_T/${REF_T}/g" wiggle.mdp
+sed -i -e "s/PCOUPL/${PCOUPL}/g" wiggle.mdp
+sed -i -e "s/PCOUPLTYPE/${PCOUPLTYPE}/g" wiggle.mdp
+sed -i -e "s/REF_P/${REF_P}/g" wiggle.mdp
+sed -i -e "s/COMPRESSIBILITY/${COMPRESSIBILITY}/g" wiggle.mdp
+sed -i -e "s/PBC/${PBC}/g" wiggle.mdp
 
 # Build Structure Based on user-defined inputs
 
