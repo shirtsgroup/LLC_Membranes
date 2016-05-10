09a5d5b01cf8e6ac05d9b56ee969fdfb878a3738
bencoscia <benjamin.coscia@colorado.edu>
2016-05-10 16:33:01 -0600

    Extending Customization to PJS.sh

--- Shell-Scripts/PJS.sh
+++ Shell-Scripts/PJS.sh
@@ -1,30 +1,188 @@
 #!/bin/bash
 # Script to build a structure to a specified number of layers using a specified
-# monomer, then energy minimize the structure, run a simulation, then readjust the box size to give a desired number of nm of water between layers, energy minimize again, and run a simulation.
-
+# monomer, then energy minimize the structure, and finally run a simulation
 # based on inputs to the .mdp files
 
-# Define Variables
+# Copy in necessary files
+ifs.sh
 
-#ifs.sh
+# Define Variables with Default values
 
-INPUT_FILE=/home/bcoscia/PycharmProjects/LLC_Structure_Builder/Monomer_Configurations/monomer4.pdb
-LAYERS=20 # kind of arbitrary but should work
-WATER_LAYER=6  # nm of water between layers in the z direction
-# Starting x and y box vectors:
-XVECT1=8.5  # for first energy minimization
-YVECT1=8.5
-XVECT2=8.5  # for second energy minimization
-YVECT2=8.5
-INCREMENT=0.1  # How much each box vector will be increased in the event of a positive energy found through energy minimzation
+# Choose which monomer to build with
+MONOMER='monomer4.pdb'  # Structure file to be used
+
+# Energy minimization parameters:
+INTEGRATOR_EM=steep  # Integrator for energy minimization
+NSTEPS_EM=50000  # Maximum number of steps to take for energy minimization
+CUTOFF_EM='verlet'  # Cut-off Scheme
+NSTLIST=10  # Neighborlist - changed automatically by gromacs unless it is set equal to 1
+
+# Membrane Dimensions
+NO_MONOMERS=6  # Number of monomers in 1 layer
+RADIUS=3  # Initial pore radius, angstroms
+PORE2PORE=40  # Pore-to-Pore distance, angstroms
+NOPORES=4  # Number of pores to be built
+DBWL=10  # Distance Between Layers
+LAYERS=20    # Number of layers wanted in the structure
+
+# Box Vector Parameters
+XVECT=8.0  # Box vector in the x direction
+YVECT=8.0  # Box vector in the y direction (not this will be multiplied by sin(120) for a monoclinic cell of 120 degrees
+INCREMENT=0.1  # Increment to increase the box vector by if there is a LINCS error
+
+# Simulation Parameters
+SIM_TITLE='Equilibration in Vacuum'  # Title of simulation
+CUTOFF_MD='verlet'  # Cut-off scheme for simulation
+INTEGRATOR_MD='md'  # Integrator type for simulation
+DT=0.002  # Time step (ps)
+WIG_LENGTH=0.01  #length of intermediate 'wiggle' simulation (ns)
+FRAMES=50  # Number of frames in trajectory
+TCOUPL='v-rescale'
+REF_T=300  # Reference Temperature, K
+PCOUPL='berendsen'
+PTYPE='semiisotropic'
+REF_P=1  # Reference Pressure, bar
+COMPRESSIBILITY=4.5e-5  # Isothermal compressibility, bar^-1
+PBC='xyz'
+
+# Solvation
+WATER_LAYER=6  # thickness (nm) between membrane layers in the z direction
+SIM_LENGTH=1  # nanoseconds
+
+# Reference for flags associated with each variable
+
+# -m  :   MONOMER ... Structure file to be used
+# -I  :   INTEGRATOR_EM ... Integrator for energy minimization
+# -s  :   NSTEPS_EM ... Maximum number of steps to take for energy minimization
+# -c  :   CUTOFF_EM ... Cut-off Scheme
+# -t  :   NSTLIST ... Neighborlist - changed automatically by gromacs unless it is set equal to 1
+# -n  :   NO_MONOMERS ... Number of monomers in 1 layer
+# -r  :   RADIUS ... Initial pore radius, angstroms
+# -p  :   PORE2PORE ... Pore-to-Pore distance, angstroms
+# -P  :   NOPORES ... Number of pores to be built
+# -w  :   DBWL ... Distance Between Layers
+# -l  :   LAYERS ... Number of layers wanted in the structure
+# -x  :   XVECT ... Box vector in the x direction
+# -y  :   YVECT ... Box vector in the y direction (not this will be multiplied by sin(120) for a monoclinic cell of 120 degrees
+# -e  :   INCREMENT ... Increment to increase the box vector by if there is a LINCS error
+# -T  :   SIM_TITLE ... Title of simulation
+# -C  :   CUTOFF_MD ... Cut-off scheme for simulation
+# -M  :   INTEGRATOR_MD ... Integrator type for simulation
+# -D  :   DT ... Time step (ps)
+# -L  :   SIM_LENGTH ... Simulation length, nanoseconds
+# -f  :   FRAMES ... Number of frames in trajectory
+# -v  :   TCOUPL ... Temperature Coupling
+# -K  :   REF_T ... Reference Temperature, K
+# -b  :   PCOUPL ... Pressure Coupling
+# -Y  :   PTYPE ... i.e. Isotropic, semiisotropic
+# -B  :   REF_P ... Reference Pressure, bar
+# -R  :   COMPRESSIBILITY ... Isothermal compressibility, bar^-1
+# -Z  :   PBC ... Periodic Boundary directions
+# -W  :   WIG_LENGTH ... length of intermediate simulation
+
+while getopts "m:I:s:c:t:n:r:p:P:w:l:x:y:e:T:C:M:D:L:f:v:K:b:Y:B:e:Z:" opt; do
+    case $opt in
+    m)  MONOMER=$OPTARG;;
+    I)  INTEGRATOR_EM=$OPTARG;;
+    s)  NSTEPS_EM=$OPTARG;;
+    c)  CUTOFF_EM=$OPTARG;;
+    t)  NSTLIST=$OPTARG;;
+    n)  NO_MONOMERS=$OPTARG;;
+    r)  RADIUS=$OPTARG;;
+    p)  PORE2PORE=$OPTARG;;
+    P)  NOPORES=$OPTARG;;
+    w)  DBWL=$OPTARG;;
+    l)  LAYERS=$OPTARG;;
+    x)  XVECT=$OPTARG;;
+    y)  YVECT=$OPTARG;;
+    e)  INCREMENT=$OPTARG;;
+    T)  SIM_TITLE=$OPTARG;;
+    C)  CUTOFF_MD=$OPTARG;;
+    M)  INTEGRATOR_MD=$OPTARG;;
+    D)  DT=$OPTARG;;
+    L)  SIM_LENGTH=$OPTARG;;
+    f)  FRAMES=$OPTARG;;
+    v)  TCOUPL=$OPTARG;;
+    K)  REF_T=$OPTARG;;
+    b)  PCOUPL=$OPTARG;;
+    Y)  PTYPE=$OPTARG;;
+    B)  REF_P=$OPTARG;;
+    R)  COMPRESSIBILITY=$OPTARG;;
+    Z)  PBC=$OPTARG;;
+    W)  WIG_LENGTH=$OPTARG;;
+    esac
+done
+
+# Calculated values based on input variables:
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"  # Directory where this script is located
+INPUT_FILE=$DIR/../Structure-Files/$MONOMER  # Full path to file where monomer structure is located
 Z_BOX_VECTOR=$((LAYERS+5)) # kind of arbitrary but should work
+MOL_LLC=$((NO_MONOMERS*NOPORES*LAYERS))  # For topology
+MOL_NA=$((NO_MONOMERS*NOPORES*LAYERS))
+NSTEPS_WIG=$(echo "$WIG_LENGTH*1000/$DT" | bc)
+NSTEPS_MD=$(echo "$SIM_LENGTH*1000/$DT" | bc)  # Number of steps to be taken during simulation to simulate the desired length of time
+NSTXOUT=$(echo "$NSTEPS_MD/$FRAMES" | bc) # Information output to trajectory
+NSTVOUT=$(echo "$NSTEPS_MD/$FRAMES" | bc)
+NSTFOUT=$(echo "$NSTEPS_MD/$FRAMES" | bc)
+NSTENERGY=$(echo "$NSTEPS_MD/$FRAMES" | bc)
+
+
+# Edit input files:
+
+# Energy Minimization
+sed -i -e "s/INTEGRATOR/${INTEGRATOR_EM}/g" em.mdp
+sed -i -e "s/NSTEPS_EM/${NSTEPS_EM}/g" em.mdp
+sed -i -e "s/CUTOFF_EM/${CUTOFF_EM}/g" em.mdp
+sed -i -e "s/NSTLIST_EM/${NSTLIST}/g" em.mdp
+
+# Wiggle.mdp
+sed -i -e "s/SIM_TITLE/${SIM_TITLE}/g" wiggle.mdp
+sed -i -e "s/CUTOFF_MD/${CUTOFF_MD}/g" wiggle.mdp
+sed -i -e "s/INTEGRATOR_MD/${INTEGRATOR_MD}/g" wiggle.mdp
+sed -i -e "s/DT/${DT}/g" wiggle.mdp
+sed -i -e "s/NSTEPS_MD/${NSTEPS_WIG}/g" wiggle.mdp
+sed -i -e "s/NSTXOUT/${NSTXOUT}/g" wiggle.mdp
+sed -i -e "s/NSTVOUT/${NSTVOUT}/g" wiggle.mdp
+sed -i -e "s/NSTFOUT/${NSTFOUT}/g" wiggle.mdp
+sed -i -e "s/NSTENERGY/${NSTENERGY}/g" wiggle.mdp
+sed -i -e "s/TCOUPL/${TCOUPL}/g" wiggle.mdp
+sed -i -e "s/REF_T/${REF_T}/g" wiggle.mdp
+sed -i -e "s/PCOUPL/${PCOUPL}/g" wiggle.mdp
+sed -i -e "s/PTYPE/${PTYPE}/g" wiggle.mdp
+sed -i -e "s/REF_P/${REF_P}/g" wiggle.mdp
+sed -i -e "s/COMPRESSIBILITY/${COMPRESSIBILITY}/g" wiggle.mdp
+sed -i -e "s/PBC/${PBC}/g" wiggle.mdp
+
+# Wiggle_solv.mdp
+sed -i -e "s/SIM_TITLE/${SIM_TITLE}/g" wiggle_solv.mdp
+sed -i -e "s/CUTOFF_MD/${CUTOFF_MD}/g" wiggle_solv.mdp
+sed -i -e "s/INTEGRATOR_MD/${INTEGRATOR_MD}/g" wiggle_solv.mdp
+sed -i -e "s/DT/${DT}/g" wiggle_solv.mdp
+sed -i -e "s/NSTEPS_MD/${NSTEPS_WIG}/g" wiggle_solv.mdp
+sed -i -e "s/NSTXOUT/${NSTXOUT}/g" wiggle_solv.mdp
+sed -i -e "s/NSTVOUT/${NSTVOUT}/g" wiggle_solv.mdp
+sed -i -e "s/NSTFOUT/${NSTFOUT}/g" wiggle_solv.mdp
+sed -i -e "s/NSTENERGY/${NSTENERGY}/g" wiggle_solv.mdp
+sed -i -e "s/TCOUPL/${TCOUPL}/g" wiggle_solv.mdp
+sed -i -e "s/REF_T/${REF_T}/g" wiggle_solv.mdp
+sed -i -e "s/PCOUPL/${PCOUPL}/g" wiggle_solv.mdp
+sed -i -e "s/PTYPE/${PTYPE}/g" wiggle_solv.mdp
+sed -i -e "s/REF_P/${REF_P}/g" wiggle_solv.mdp
+sed -i -e "s/COMPRESSIBILITY/${COMPRESSIBILITY}/g" wiggle_solv.mdp
+sed -i -e "s/PBC/${PBC}/g" wiggle_solv.mdp
+
+# NaPore.top
+sed -i -e "s/MOL_LLC/${MOL_LLC}/g" NaPore.top
+sed -i -e "s/MOL_NA/${MOL_NA}/g" NaPore.top
+
 # Build Structure Based on user-defined inputs
 
-python /home/bcoscia/PycharmProjects/LLC_Structure_Builder/Structure_Builder_for_Bash.py -i $INPUT_FILE -l $LAYERS >> initial.gro
+python $DIR/../Structure_Builder/Structure_Builder_for_Bash.py -i $INPUT_FILE -l $LAYERS -m $NO_MONOMERS -r $RADIUS -p $PORE2PORE -n $NOPORES -d $DBWL >> initial.gro
 
 # Put the structure in a box
 
-gmx editconf -f initial.gro -o box.gro -c -bt triclinic -box $XVECT1 $YVECT1 $Z_BOX_VECTOR -angles 90 90 120
+gmx editconf -f initial.gro -o box.gro -c -bt triclinic -box $XVECT $YVECT $Z_BOX_VECTOR -angles 90 90 120
 
 # Prepare input file (.tpr) for energy minimization
 
@@ -40,9 +198,9 @@ ENERGY1=$(cat box_em.log | grep 'Potential Energy' | awk '{print substr($0,21,5}
 # If the potential energy is positive, then the box vector is incremented by a fixed amount until the energy comes out negative
 
 while [ $(echo " $ENERGY1 > 0" | bc) -eq 1 ]; do
-        XVECT1=$(echo "$XVECT1 + $INCREMENT" | bc -l)
-        YVECT1=$(echo "$YVECT1 + $INCREMENT" | bc -l)
-        gmx editconf -f initial.gro -o box.gro -c -bt triclinic -box $XVECT1 $YVECT1 $THICKNESS -angles 90 90 120
+        XVECT1=$(echo "$XVECT + $INCREMENT" | bc -l)
+        YVECT1=$(echo "$YVECT + $INCREMENT" | bc -l)
+        gmx editconf -f initial.gro -o box.gro -c -bt triclinic -box $XVECT $YVECT $THICKNESS -angles 90 90 120
         gmx grompp -f em.mdp -c box.gro -p NaPore.top -o box_em.tpr
         gmx mdrun -v -deffnm box_em
 	ENERGY1=$(cat box_em.log | grep 'Potential Energy' | awk '{print substr($0,21,5)}')
@@ -58,7 +216,7 @@ gmx mdrun  -v -deffnm wiggle_vac
 
 INPUT_VAC_FILE=wiggle_vac.gro
 
-THICKNESS=$(python /home/bcoscia/PycharmProjects/Membrane_Dimensions/Thickness_Bash.py -i $INPUT_VAC_FILE -w $WATER_LAYER)
+THICKNESS=$(python /$DIR/../Data-Analysis/Thickness_Bash.py -i $INPUT_VAC_FILE -w $WATER_LAYER)
 
 # Edit box to correct dimensions (i.e. so there is room for the specified amount of water)
 
@@ -76,9 +234,9 @@ ENERGY2=$(cat em_new_box.log | grep 'Potential Energy' | awk '{print substr($0,2
 # if the potential energy is positive, then the box vector is increased by a defined increment until the energy is negative after energy minimzation
 
 while [ $(echo "  $ENERGY2 > 0" | bc) -eq 1 ]; do
-        XVECT2=$(echo "$XVECT2 + $INCREMENT" | bc -l)
-        YVECT2=$(echo "$YVECT2 + $INCREMENT" | bc -l)
-        gmx editconf -f wiggle_vac.gro -o new_box.gro -c -bt triclinic -box $XVECT2 $YVECT2 $THICKNESS -angles 90 90 60
+        XVECT2=$(echo "$XVECT + $INCREMENT" | bc -l)
+        YVECT2=$(echo "$YVECT + $INCREMENT" | bc -l)
+        gmx editconf -f wiggle_vac.gro -o new_box.gro -c -bt triclinic -box $XVECT $YVECT $THICKNESS -angles 90 90 60
         gmx grompp -f em.mdp -c new_box.gro -p NaPore.top -o em_new_box.tpr
         gmx mdrun -v -deffnm em_new_box
 	ENERGY2=$(cat em_new_box.log | grep 'Potential Energy' | awk '{print substr($0,21,5)}')
