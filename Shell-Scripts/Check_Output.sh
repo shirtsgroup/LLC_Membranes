91b99198147b9c1fd9574a1fb88321d2514d49a5
bencoscia <benjamin.coscia@colorado.edu>
2016-07-12 13:56:33 -0600

    Debugging on Janus. Job array now contains job ids for jobs that might not be in queue because they ended recently. Completed jobs are erased from the job_array and then extended

--- Shell-Scripts/Check_Output.sh
+++ Shell-Scripts/Check_Output.sh
@@ -58,11 +58,19 @@ fi
 # JOB_ARRAY+=($(sbatch Run_Janus.sh | cut -b 21,22,23,24,25,26,27))
 # or create an array automatically:
 
-JOB_ARRAY=()  # initialize JOB_ARRAY
 squeue --user=$USER > queue  # save the queue output to a file
 NO_LINES=$(wc -l < queue)  # find the number of lines in queue
-for i in ${seq 2 $NO_LINES}; do  # add the job id for all jobs in the user's queue to JOB_ARRAY. Exclude the first line
-    JOB_ARRAY+=($(sed "${i}q;d" queue | cut -c 12-18))
+for i in $(seq 2 $NO_LINES); do  # add the job id for all jobs in the user's queue to JOB_ARRAY. Exclude the first line
+    echo $(sed "${i}q;d" queue | cut -c 12-18) >> job_array
+done
+
+sort job_array | uniq > job_array_uniq # get rid of duplicates
+
+JOB_ARRAY=()  # intialize job array
+ENTRIES=$(wc -l < job_array_uniq)  # find the number of lines in queue
+for i in $(seq 1 ${ENTRIES}); do  # add the job id for all jobs in the user's queue to JOB_ARRAY. Exclude the first line
+    JOB_ARRAY+=($(sed "${i}q;d" job_array_uniq | cut -c 1-7))
+done
 
 # Remove any exclusions
 for i in ${EXCLUSIONS[@]}; do
@@ -74,7 +82,7 @@ for JOB_ID in ${JOB_ARRAY[@]}; do  # look at all running jobs
     squeue --user=$USER > queue  # save the queue information to a temporary file called queue
     LINE=$(sed -n "/${JOB_ID}/=" queue)  # find the line containing the job_id of interest
     if [[ -z ${LINE} ]]; then  # if $LINE is empty, meaning it doesn't exist in the queue, then the job is probably done
-        : # do nothing and move on to extending the job
+        sed -i "/${JOB_ID}/d" -i job_array # remove all instances of that job_id from job_array
     else
         STATUS=$(sed "${LINE}q;d" queue | cut -b 48,49)  # search that line to find the job's status
 
@@ -96,11 +104,13 @@ for JOB_ID in ${JOB_ARRAY[@]}; do  # look at all running jobs
 
     CHECK=0
 
-    # We need a log file from a previous iteration for comparison. If this is the first iteration, then one does not
+    # We need a log file from a previous iteration for comparison. If the job just started, then one does not
     # exist. I make a dummy file below so that log_${JOB_ID} and log_${JOB_ID}_prev will be different which will prevent
     # the script from extending and restarting the job every time
-    if [ ${ITERATION}==1 ]; then  # if it's the first iteration
-        echo 'Placeholder' > log_${JOB_ID}_prev  # make a placeholding log_${JOB_ID}_prev
+    if [ -e "log_${JOB_ID}_prev" ]; then  # check for the existence of the file log_${JOB_ID}_prev
+        :  # if it exists, do nothing
+    else
+        echo 'Placeholder' > log_${JOB_ID}_prev  # if it doesn't exist, make a place holding log_${JOB_ID}_prev file
     fi
 
     cmp log_${JOB_ID} log_${JOB_ID}_prev || export CHECK=1  # if the two files are different, CHECK=1 ... this is what we want unless the job
@@ -127,7 +137,7 @@ for JOB_ID in ${JOB_ARRAY[@]}; do  # look at all running jobs
             echo "ml numpy" >> ${WORKDIR}/Extend_Sim.sh
             echo >> ${WORKDIR}/Extend_Sim.sh
             echo "mpirun -np ${NP} gmx_mpi mdrun -s ${TPR} -cpi ${CPT} -v -deffnm "${TPR//.tpr}"" >> ${WORKDIR}/Extend_Sim.sh
-            sbatch Extend_Sim.sh  # submit job
+            sbatch ${WORKDIR}/Extend_Sim.sh  # submit job
         elif [ ${RESOURCE}=='bridges' ]; then  # write a bridges batch submission script
             echo "#!/bin/bash" > ${WORKDIR}/Extend_Sim.sh
             echo >> ${WORKDIR}/Extend_Sim.sh
@@ -145,7 +155,7 @@ for JOB_ID in ${JOB_ARRAY[@]}; do  # look at all running jobs
             echo "echo "$SLURM_NPROCS=" $SLURM_NPROCS" >> ${WORKDIR}/Extend_Sim.sh
             echo >> ${WORKDIR}/Extend_Sim.sh
             echo "mpirun -np ${NP} gmx_mpi mdrun -s ${TPR} -cpi ${CPT} -v -deffnm "${TPR//.tpr}"" >> ${WORKDIR}/Extend_Sim.sh
-            sbatch Extend_Sim.sh # submit job
+            sbatch ${SBATCH}/Extend_Sim.sh # submit job
         fi
     fi
     ITERATION=$((ITERATION+1))  # increment iteration variable
--- Shell-Scripts/test.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-echo "#SBATCH --qos janus-long" > slurm.sh
-echo "#SBATCH --nodes 8" >> slurm.sh
-echo "#SBATCH --ntasks-per-node 1" >> slurm.sh
-echo "#SBATCH --time 168:00:00" >> slurm.sh
-
