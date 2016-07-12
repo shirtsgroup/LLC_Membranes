79d04751b25b50c4f28b29b20c9810503b085e30
bencoscia <benjamin.coscia@colorado.edu>
2016-07-12 14:59:30 -0600

    - fixed paths so job_array, queue, dir etc are output to a chosen directory (scratch on Janus) - moved gromacs command into batch submission script

--- Shell-Scripts/Check_Output.sh
+++ Shell-Scripts/Check_Output.sh
@@ -12,7 +12,7 @@ SEC=00  # seconds requested for sim extension
 NODES=8  # number of nodes requested
 
 while getopts "e:n:t:c:l:f:j:x:r:h:m:s" opt; do
-    case $opt in
+    case ${opt} in
     e) EXTENSION=$OPTARG;;
     t) TPR=$OPTARG;;
     c) CPT=$OPTARG;;
@@ -34,6 +34,7 @@ if [ ${RESOURCE}=='janus' ]; then
     NTASKSPERNODE=1
     NP=$((NODES*2))
     GMX_PATH="/projects/beco4952/pkgs/gromacs/5.1.2/bin"
+    OUTPUT="/lustre/janus_scratch/beco4952/Cron_Output"  # place where files like job_array, queue and dir will be stored
     if (( ${HOURS}>24 )); then
         QOS='janus-long'
     else
@@ -59,18 +60,18 @@ fi
 # JOB_ARRAY+=($(sbatch Run_Janus.sh | cut -b 21,22,23,24,25,26,27))
 # or create an array automatically:
 
-squeue --user=$USER > queue  # save the queue output to a file
-NO_LINES=$(wc -l < queue)  # find the number of lines in queue
+squeue --user=$USER > ${OUTPUT}/queue  # save the queue output to a file
+NO_LINES=$(wc -l < ${OUTPUT}/queue)  # find the number of lines in queue
 for i in $(seq 2 $NO_LINES); do  # add the job id for all jobs in the user's queue to JOB_ARRAY. Exclude the first line
-    echo $(sed "${i}q;d" queue | cut -c 12-18) >> job_array
+    echo $(sed "${i}q;d" ${OUTPUT}/queue | cut -c 12-18) >> ${OUTPUT}/job_array
 done
 
-sort job_array | uniq > job_array_uniq # get rid of duplicates
+sort ${OUTPUT}/job_array | uniq > ${OUTPUT}/job_array_uniq # get rid of duplicates
 
 JOB_ARRAY=()  # intialize job array
-ENTRIES=$(wc -l < job_array_uniq)  # find the number of lines in queue
+ENTRIES=$(wc -l < ${OUTPUT}/job_array_uniq)  # find the number of lines in queue
 for i in $(seq 1 ${ENTRIES}); do  # add the job id for all jobs in the user's queue to JOB_ARRAY. Exclude the first line
-    JOB_ARRAY+=($(sed "${i}q;d" job_array_uniq | cut -c 1-7))
+    JOB_ARRAY+=($(sed "${i}q;d" ${OUTPUT}/job_array_uniq | cut -c 1-7))
 done
 
 # Remove any exclusions
@@ -79,13 +80,13 @@ for i in ${EXCLUSIONS[@]}; do
 done
 
 for JOB_ID in ${JOB_ARRAY[@]}; do  # look at all running jobs
-    squeue --user=$USER > queue  # save the queue information to a temporary file called queue
-    LINE=$(sed -n "/${JOB_ID}/=" queue)  # find the line containing the job_id of interest
+    squeue --user=$USER > ${OUTPUT}/queue  # save the queue information to a temporary file called queue
+    LINE=$(sed -n "/${JOB_ID}/=" ${OUTPUT}/queue)  # find the line containing the job_id of interest
     if [[ -z ${LINE} ]]; then  # if $LINE is empty, meaning it doesn't exist in the queue, then the job is probably done
-        sed -i "/${JOB_ID}/d" -i job_array # remove all instances of that job_id from job_array
+        sed -i "/${JOB_ID}/d" -i ${OUTPUT}/job_array # remove all instances of that job_id from job_array
         DONE=1
     else
-        STATUS=$(sed "${LINE}q;d" queue | cut -b 48,49)  # search that line to find the job's status
+        STATUS=$(sed "${LINE}q;d" ${OUTPUT}/queue | cut -b 48,49)  # search that line to find the job's status
 
         if [ ${STATUS} != ' R' ]; then  # if the job is not running but in the queue or completing, leave it. Exit the whole script
             exit
@@ -95,29 +96,29 @@ for JOB_ID in ${JOB_ARRAY[@]}; do  # look at all running jobs
     # This point in the loop is reached only if the job is running or finished
 
     # figure out the working directory for the job ID
-    scontrol show job ${JOB_ID} > dir  # save all the information pertaining to the job id to a file
-    WORKDIRLINE=$(sed -n '/WorkDir/=' dir)  # find the line containing the working directory for the job
-    WORKDIRFULL=$(sed "${WORKDIRLINE}q;d" dir)  # save the entire line
-    WORKDIR=$(sed "${WORKDIRLINE}q;d" dir | cut -c 12-${#WORKDIRFULL})  # cut from the 12th character to the length of the line
+    scontrol show job ${JOB_ID} > ${OUTPUT}/dir  # save all the information pertaining to the job id to a file
+    WORKDIRLINE=$(sed -n '/WorkDir/=' ${OUTPUT}/dir)  # find the line containing the working directory for the job
+    WORKDIRFULL=$(sed "${WORKDIRLINE}q;d" ${OUTPUT}/dir)  # save the entire line
+    WORKDIR=$(sed "${WORKDIRLINE}q;d" ${OUTPUT}/dir | cut -c 12-${#WORKDIRFULL})  # cut from the 12th character to the length of the line
 
     # We need to monitor the currently running job to see if it is still going, or if it is stalled/finished
-    tail ${WORKDIR}/${LOG} > log_${JOB_ID}  # Save the last 10 lines of the .log file to log_prev. This will be used as a 1st comparison
+    tail ${WORKDIR}/${LOG} > ${WORKDIR}/log_${JOB_ID}  # Save the last 10 lines of the .log file to log_prev. This will be used as a 1st comparison
 
     CHECK=0
 
     # We need a log file from a previous iteration for comparison. If the job just started, then one does not
     # exist. I make a dummy file below so that log_${JOB_ID} and log_${JOB_ID}_prev will be different which will prevent
     # the script from extending and restarting the job every time
-    if [ -e "log_${JOB_ID}_prev" ]; then  # check for the existence of the file log_${JOB_ID}_prev
+    if [ -e "${WORKDIR}/log_${JOB_ID}_prev" ]; then  # check for the existence of the file log_${JOB_ID}_prev
         :  # if it exists, do nothing
     else
-        echo 'Placeholder' > log_${JOB_ID}_prev  # if it doesn't exist, make a place holding log_${JOB_ID}_prev file
+        echo 'Placeholder' > ${WORKDIR}/log_${JOB_ID}_prev  # if it doesn't exist, make a place holding log_${JOB_ID}_prev file
     fi
 
-    cmp log_${JOB_ID} log_${JOB_ID}_prev || export CHECK=1  # if the two files are different, CHECK=1 ... this is what we want unless the job
+    cmp ${WORKDIR}/log_${JOB_ID} ${WORKDIR}/log_${JOB_ID}_prev || export CHECK=1  # if the two files are different, CHECK=1 ... this is what we want unless the job
     # is finished. Otherwise (CHECK=0), it is an indication of a job that needs to be restarted (a stalled or finished job)
     # now change log to the new log_prev for the next iteration
-    mv log_${JOB_ID} log_${JOB_ID}_prev
+    mv ${WORKDIR}/log_${JOB_ID} ${WORKDIR}/log_${JOB_ID}_prev
 
     if [ $CHECK==0 ]; then  # if the files are the same (meaning output is no longer being written)
 
@@ -126,8 +127,6 @@ for JOB_ID in ${JOB_ARRAY[@]}; do  # look at all running jobs
         else
             scancel ${JOB_ID}  # run this command to cancel the job only if the job is still running
         fi
-
-        ${GMX_PATH}/gmx_mpi convert-tpr -s ${TPR} -extend ${EXTENSION} -o ${TPR}  # make a new, extended .tpr file
 
         if [ ${RESOURCE}=='janus' ]; then  # write a janus batch submission script
             echo "#! /bin/bash" > ${WORKDIR}/Extend_Sim.sh
@@ -142,6 +141,7 @@ for JOB_ID in ${JOB_ARRAY[@]}; do  # look at all running jobs
             echo "ml python/2.7.10" >> ${WORKDIR}/Extend_Sim.sh
             echo "ml numpy" >> ${WORKDIR}/Extend_Sim.sh
             echo >> ${WORKDIR}/Extend_Sim.sh
+            echo "gmx_mpi convert-tpr -s ${TPR} -extend ${EXTENSION} -o ${TPR}" >> ${WORKDIR}/Extend_Sim.sh
             echo "mpirun -np ${NP} gmx_mpi mdrun -s ${TPR} -cpi ${CPT} -v -deffnm "${TPR//.tpr}"" >> ${WORKDIR}/Extend_Sim.sh
             sbatch ${WORKDIR}/Extend_Sim.sh  # submit job
         elif [ ${RESOURCE}=='bridges' ]; then  # write a bridges batch submission script
@@ -160,8 +160,9 @@ for JOB_ID in ${JOB_ARRAY[@]}; do  # look at all running jobs
             echo "cd $SLURM_SUBMIT_DIR" >> ${WORKDIR}/Extend_Sim.sh
             echo "echo "$SLURM_NPROCS=" $SLURM_NPROCS" >> ${WORKDIR}/Extend_Sim.sh
             echo >> ${WORKDIR}/Extend_Sim.sh
+            echo "gmx_mpi convert-tpr -s ${TPR} -extend ${EXTENSION} -o ${TPR}" >> ${WORKDIR}/Extend_Sim.sh
             echo "mpirun -np ${NP} gmx_mpi mdrun -s ${TPR} -cpi ${CPT} -v -deffnm "${TPR//.tpr}"" >> ${WORKDIR}/Extend_Sim.sh
-            sbatch ${SBATCH}/Extend_Sim.sh # submit job
+            sbatch ${WORKDIR}/Extend_Sim.sh # submit job
         fi
     fi
 done
