22fb0763f3326da36eb2d5fad90bb16e4632d30e
bencoscia <benjamin.coscia@colorado.edu>
2016-07-14 17:31:29 -0600

    debugging

--- Shell-Scripts/Check_Output.sh
+++ Shell-Scripts/Check_Output.sh
@@ -86,11 +86,13 @@ done
 # Remove any exclusions
 for i in ${EXCLUSIONS[@]}; do
    JOB_ARRAY=( "${JOB_ARRAY[@]/$i}" )
+   echo $i > ${OUTPUT}/looparray
 done
 
 for JOB_ID in ${JOB_ARRAY[@]}; do  # look at all running jobs
     squeue --user=$USER > ${OUTPUT}/queue  # save the queue information to a temporary file called queue
-    LINE=$(sed -n "/${JOB_ID}/=" ${OUTPUT}/queue) > ${OUTPUT}/line # find the line containing the job_id of interest
+    LINE=$(sed -n "/${JOB_ID}/=" ${OUTPUT}/queue) # find the line containing the job_id of interest
+    echo $LINE > ${OUTPUT}/line
     if [[ -z ${LINE} ]]; then  # if $LINE is empty, meaning it doesn't exist in the queue, then the job is probably done
         sed -i "/${JOB_ID}/d" -i ${OUTPUT}/job_array # remove all instances of that job_id from job_array
         DONE=1
