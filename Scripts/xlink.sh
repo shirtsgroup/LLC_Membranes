40aef90568f730f62c01c96b4b4b8ca0330c4bb8
bencoscia <benjamin.coscia@colorado.edu>
2016-08-22 12:46:12 -0600

    xlink.sh can be continued from a specific iteration. Backup files are deleted on every iteration

--- Scripts/xlink.sh
+++ Scripts/xlink.sh
@@ -13,8 +13,10 @@ DEGREE=.9  # Degree of crosslinking
 NO_MONOMERS=480
 NO_TAILS=3
 MONOMER="monomer2"
+ITERATION=0  # starting iteration
+STOP=0
 
-while getopts "c:t:g:d:s:x:f:m" opt; do
+while getopts "c:t:g:d:s:x:f:m:C:" opt; do
     case $opt in
     c) CUTOFF=$OPTARG;;
     t) TERM_PROB=$OPTARG;;
@@ -24,11 +26,17 @@ while getopts "c:t:g:d:s:x:f:m" opt; do
     x) XLINKS=$OPTARG;;
     f) FRAMES=$OPTARG;;
     m) MONOMER=$OPTARG;;
+    C) ITERATION=$OPTARG;;  #if the sript is being continued
     esac
 done
 
-ITERATION=0  # starting iteration
-STOP=0
+if [ ${ITERATION} != 0 ]; then
+    ITERATION=$((ITERATION-1))
+    XLINKS=$(tail xlink_${ITERATION}.log -n 2 | head -n 1 | cut -c 19-22)
+    TERM=$(tail -n 6 xlink_${ITERATION}.log | head -n 1 | cut -c 26-29)
+    STOP=$(tail -n 1 xlink_${ITERATION}.log | cut -c 26)
+    ITERATION=$((ITERATION+1))
+fi
 
 Write_Input.py -x on -L ${SIM_LENGTH} -D 0.001 -f ${FRAMES} # -I cg
 
@@ -49,6 +57,7 @@ while [ ${STOP} -eq 0 ]; do
     STOP=$(tail -n 1 xlink_${ITERATION}.log | cut -c 26)
     ITERATION=$((ITERATION+1))
     echo ${TERM}
+    if test -f "\#*"; then rm \#*; fi  # remove backups if they exist
 done
 
 xlink.py -i ${GRO} -c ${CUTOFF}  -e ${TERM_PROB} -r ${ITERATION} -d ${CUTOFF_RAD} -y crosslinked_new.itp -x ${XLINKS} -S 'yes'
