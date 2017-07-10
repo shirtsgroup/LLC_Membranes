a5ed0ed9efbf178fd1b1d5524c5562df96b7c1d8
Benjamin Coscia <bjc@br006.pvt.bridges.psc.edu>
2017-07-10 12:54:21 -0400

    Create and submit new batch script with same node configuration

--- Scripts/kill.sh
+++ Scripts/kill.sh
@@ -10,8 +10,15 @@ for i in $jobs; do  # check all jobs
 	if [ -f ${path} ]; then  # check if the file at the given path exists. If it doesn't exist then job hasn't started running yet
 		if grep -q 'Fatal' ${path}; then # Check if there are LINCS warnings
 			scancel $i  # cancel the job because it is probably stopped
-			DIR=$(dirname "${path}")
-			sbatch ${DIR}/Run.sh
+			DIR=$(dirname "${path}") # directory where job is located
+			KEEP=$(grep -n 'module' ${DIR}/Run.sh | tail -n1 | cut -d: -f1)  # keep up to this line from the original Run.sh file
+			head -n ${KEEP} ${DIR}/Run.sh > ${DIR}/continue.sh  # copy lines up to KEEP into a new batch script
+			processes=$(get_node_config.py -i Run.sh)  # number of processes
+			echo '' >> ${DIR}/continue.sh
+			echo "mpirun -np ${processes} gmx_mpi mdrun -s wiggle.tpr -cpi wiggle.cpt -append -v -deffnm wiggle" >> continue.sh # command to continue where the simulation left off
+			sbatch ${DIR}/continue.sh  # submit job for continuation
+			time=$(date '+%H:%M')
+			mail -s "Job ${i} Cancelled at $time" benjamin.coscia@colorado.edu <<< "Path to job failure: ${path}"  # email myself the location of failed job
 		fi
 	fi
 done
