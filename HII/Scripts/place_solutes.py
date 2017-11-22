6ce63aead30dca429c483b6b8a7c2ffd6eb7f09b
bencoscia <benjamin.coscia@colorado.edu>
2017-11-22 09:43:36 -0700

    updates to outline and debugging

--- HII/Scripts/place_solutes.py
+++ HII/Scripts/place_solutes.py
@@ -28,6 +28,7 @@ def initialize():
     parser.add_argument('--solute_path', default='/home/bcoscia/PycharmProjects/GitHub/HII/top/solutes', help='path to '
                         'where solute coordinate files are held')
     parser.add_argument('-l', '--layers', default=20, type=int, help='Number of layers')
+    parser.add_argument('-nn', '--neighbors', default=3, type=int, help='Number of nearest neighbor water molecules to remove')
 
     args = parser.parse_args()
 
@@ -64,19 +65,19 @@ def trace_pores(pos, box, layers):
     :return: points which trace the pore center
     """
 
-    atoms_p_pore = int(pos.shape[0] / 4)
-    atoms_p_layer = int(atoms_p_pore / layers)
+    atoms_p_pore = int(pos.shape[0] / 4)  # atoms in each pore
+    atoms_p_layer = int(atoms_p_pore / layers)  # atom per layer
 
     v = np.zeros([4, 2])  # vertices of unitcell box
     v[0, :] = [0, 0]
     v[1, :] = [box[0, 0], 0]
     v[3, :] = [box[1, 0], box[1, 1]]
     v[2, :] = v[3, :] + [box[0, 0], 0]
-    center = [np.mean(v[:, 0]), np.mean(v[:, 1]), 0]
-    bounds = path.Path(v)
+    center = [np.mean(v[:, 0]), np.mean(v[:, 1]), 0]  # geometric center of box
+    bounds = path.Path(v)  # create a path tracing the vertices, v
 
-    angle = np.arccos(box[1, 1]/box[0, 0])
-    if box[1, 0] < 0:
+    angle = np.arccos(box[1, 1]/box[0, 0])  # angle of monoclinic box
+    if box[1, 0] < 0:  # the case of an obtuse angle
         angle += np.pi / 2
 
     m = (v[3, 1] - v[0, 1]) / (v[3, 0] - v[0, 0])  # slope from points connecting first and third vertices
@@ -89,27 +90,44 @@ def trace_pores(pos, box, layers):
             before = pore[l*atoms_p_layer, :]  # choose the first atom as a reference
             shift = transform.translate(pore[l*atoms_p_layer:(l+1)*atoms_p_layer, :], before, center)  # shift everything to towards the center
 
-            for i in range(shift.shape[0]):
+            for i in range(shift.shape[0]):  # check if the points are within the bounds of the unitcell
                 if not bounds.contains_point(shift[i, :2]):
-                    shift[i, :] = put_in_box(shift[i, :], box[0, 0], box[1, 1], m, angle)
+                    shift[i, :] = put_in_box(shift[i, :], box[0, 0], box[1, 1], m, angle)  # if its not in the unitcell, shift it so it is
 
             c = np.zeros([1, 3])
-            c[0, :] = [np.mean(shift[:, 0]), np.mean(shift[:, 1]), np.mean(shift[:, 2])]
-            centers[p*layers + l, :] = transform.translate(c, center, before)
+            c[0, :] = [np.mean(shift[:, 0]), np.mean(shift[:, 1]), np.mean(shift[:, 2])]  # geometric center of reference atoms in this layer
+            centers[p*layers + l, :] = transform.translate(c, center, before)  # move everything back to where it was
 
-            if not bounds.contains_point(centers[p*layers, :]):
+            if not bounds.contains_point(centers[p*layers, :]):  # make sure everything is in the box again
                 centers[p*layers + l, :] = put_in_box(centers[p*layers, :], box[0, 0], box[1, 1], m, angle)
 
     return centers
 
 
-def placement(z, pts):
+def placement(z, pts, box):
     """
     :param z: z location where solute should be placed
     :param pts: points which run through the pore
     :return: location to place solute
     """
 
+    v = np.zeros([4, 2])  # vertices of unitcell box
+    v[0, :] = [0, 0]
+    v[1, :] = [box[0, 0], 0]
+    v[3, :] = [box[1, 0], box[1, 1]]
+    v[2, :] = v[3, :] + [box[0, 0], 0]
+    center = [np.mean(v[:, 0]), np.mean(v[:, 1]), 0]  # geometric center of box
+    bounds = path.Path(v)  # create a path tracing the vertices, v
+
+    angle = np.arccos(box[1, 1]/box[0, 0])  # angle of monoclinic box
+    if box[1, 0] < 0:  # the case of an obtuse angle
+        angle += np.pi / 2
+
+    m = (v[3, 1] - v[0, 1]) / (v[3, 0] - v[0, 0])  # slope from points connecting first and third vertices
+
+    # shift = transform.translate(z, before, center)
+    #
+    # put_in_box(pt, box[0, 0], box[1, 1], m, angle)
     lower = 0
     while pts[lower, 2] < z:
         lower += 1
@@ -118,14 +136,28 @@ def placement(z, pts):
     while pts[upper, 2] > z:
         upper -= 1
 
+    limits = np.zeros([2, 3])
+    limits[0, :] = pts[lower, :]
+    limits[1, :] = pts[upper, :]
+    shift = transform.translate(limits, limits[0, :], center)
+    for i in range(shift.shape[0]):  # check if the points are within the bounds of the unitcell
+        if not bounds.contains_point(shift[i, :2]):
+            shift[i, :] = put_in_box(shift[i, :], box[0, 0], box[1, 1], m, angle)
+
     # Use parametric representation of line between upper and lower points to find the xy value where z is satsified
+    v = shift[1, :] - shift[0, :]  # direction vector
+    t = (0 - shift[0, 2]) / v[2]  # solve for t since we know z
+    x = shift[0, 0] + t*v[0]
+    y = shift[0, 1] + t*v[1]
 
-    v = pts[upper, :] - pts[lower, :]  # direction vector
-    t = (z - pts[lower, 2]) / v[2]  # solve for t since we know z
-    x = pts[lower, 0] + t*v[0]
-    y = pts[lower, 1] + t*v[1]
+    place = np.zeros([1, 3])
+    place[0, :] = [x, y, 0]
+    place = transform.translate(place, center, limits[0, :])
 
-    return [x, y, z]
+    if not bounds.contains_point(place[0, :]):  # make sure everything is in the box again
+        place[0, :] = put_in_box(place[0, :], box[0, 0], box[1, 1], m, angle)
+
+    return place
 
 
 if __name__ == "__main__":
@@ -133,26 +165,11 @@ if __name__ == "__main__":
     args = initialize()
 
     t = md.load(args.gro)  # load trajectory
-    xyz = t.xyz[0, :, :]
-
-    names = [a.name for a in t.topology.atoms]
-    res = [a.residue.name for a in t.topology.atoms]
-
-    # change names and residues back to be consistent with tip3p water model. idk mdtraj does this to me D':
-    for i, name in enumerate(res):
-        if name == "HOH":
-            res[i] = 'SOL'
-            if names[i] == 'O':
-                names[i] = 'OW'
-            elif names[i] == 'H1':
-                names[i] = 'HW1'
-            elif names[i] == 'H2':
-                names[i] = 'HW2'
+    xyz = t.xyz[0, :, :]  # positions of all atoms
 
     full_box = t.unitcell_vectors
     box = t.unitcell_vectors[0, :2, :2]  # get xy box vectors
     zmax, zmin = physical.thickness(args.gro, args.ref, grid=False)[1:3]  # find limits for solute placement
-    z_placement = np.linspace(zmin, zmax, args.number + 2)[1:-1]  # points along z axis where solutes will be placed (exclude end points ... for now)
 
     angle = np.arccos(box[1, 1]/box[0, 0])
     if box[1, 0] < 0:
@@ -174,24 +191,66 @@ if __name__ == "__main__":
     com = np.zeros([3])  # center of mass of solute
     for i in range(solute.xyz.shape[1]):
         com += solute.xyz[0, i, :]
-
     com /= solute.xyz.shape[1]
-    pores = int(centers.shape[0] / args.layers)
-    for i in range(pores):
-        for j in range(args.number):
-            place = placement(z_placement[j], centers[i*args.layers:(i+1)*args.layers, :])
-            xyz = np.concatenate((xyz, transform.translate(solute.xyz[0, :, :], com, place)))
 
-    box_gromacs = [full_box[0, 0, 0], full_box[0, 1, 1], full_box[0, 2, 2], full_box[0, 0, 1], full_box[0, 2, 0],
-                   full_box[0, 1, 0], full_box[0, 0, 2], full_box[0, 1, 2], full_box[0, 2, 0]]
+    pores = int(centers.shape[0] / args.layers)
+    solute_locations = np.zeros([pores*args.number, 3])
+    solute_coords = np.zeros([solute.xyz.shape[1]*args.number*pores, 3])
+    for i in range(pores):
+        zmin = np.min(centers[i*args.layers:(i+1)*args.layers, 2])
+        zmax = np.max(centers[i*args.layers:(i+1)*args.layers, 2])
+        z_placement = np.linspace(zmin, zmax, args.number + 2)[1:-1]  # points along z axis where solutes will be placed (exclude end points ... for now)
+        for j in range(args.number):
+            place = placement(z_placement[j], centers[i*args.layers:(i+1)*args.layers, :], box)
+            solute_locations[i*args.number + j, :] = place
+            solute_coords[(i*args.number + j)*solute.xyz.shape[1]:(i*args.number + j + 1)*solute.xyz.shape[1], :] = \
+                transform.translate(solute.xyz[0, :, :], com, place[0, :])
+            # xyz = np.concatenate((xyz, transform.translate(solute.xyz[0, :, :], com, place[0, :])))
+
+    # remove waters close to inserted solutes
+    water = [a.index for a in t.topology.atoms if a.residue.name == 'HOH' and a.name == 'O']
+
+    tree = spatial.cKDTree(xyz[water, :])
+    rm = []
+    for i in range(solute_locations.shape[0]):
+        for j in tree.query(solute_locations[i, :], k=args.neighbors)[1]:
+            rm.append(water[j])
+            rm.append(water[j] + 1)
+            rm.append(water[j] + 2)
+
+    keep = [a.index for a in t.topology.atoms if a.residue.name != 'HOH']
+    for i in water:
+        if i not in rm:
+            keep.append(i)
+            keep.append(i + 1)
+            keep.append(i + 2)
+
+    pos = np.concatenate((xyz[keep, :], solute_coords))
+
+    names = [a.name for a in t.topology.atoms if a.index not in rm]  # names of all atoms
+    res = [a.residue.name for a in t.topology.atoms if a.index not in rm]  # residue names of all atoms
+
+    # change names and residues back to be consistent with tip3p water model. idk why mdtraj does this to me D':
+    for i, name in enumerate(res):
+        if name == "HOH":
+            res[i] = 'SOL'
+            if names[i] == 'O':
+                names[i] = 'OW'
+            elif names[i] == 'H1':
+                names[i] = 'HW1'
+            elif names[i] == 'H2':
+                names[i] = 'HW2'
 
     ids = names + solute_atom_names*args.number*4
     res_names = res + solute_resnames*args.number*4
 
-    file_rw.write_gro_pos(xyz, 'solutes.gro', box=box_gromacs, ids=ids, res=res_names)
+    box_gromacs = [full_box[0, 0, 0], full_box[0, 1, 1], full_box[0, 2, 2], full_box[0, 0, 1], full_box[0, 2, 0],
+                   full_box[0, 1, 0], full_box[0, 0, 2], full_box[0, 1, 2], full_box[0, 2, 0]]
+    print(pos.shape)
+    print(len(res_names))
+    file_rw.write_gro_pos(pos, 'solutes.gro', box=box_gromacs, ids=ids, res=res_names)
 
     # update topology
-
     topology = []
     with open('%s' % args.top, 'r') as f:
         for line in f:
@@ -204,6 +263,13 @@ if __name__ == "__main__":
     topology.insert(system_index, ';%s Topology\n' % args.solute)
     topology.insert(system_index + 1, '#include "%s/%s.itp"\n' % (args.solute_path, args.solute))
     topology.insert(system_index + 2, '\n')
+
+    # modify number of water molecules since some were removed
+    sol = 0
+    while topology[sol].count('SOL') == 0:
+        sol += 1
+
+    topology[sol] = '%s                 %d\n' %('SOL', int(int(topology[sol].split()[1]) - args.neighbors*args.number*pores))
     topology.append('%s                 %s' % (solute_resnames[0], args.number*4))
 
     with open('solute.top', 'w') as f:
