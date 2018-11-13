9ccb546a0da60f27d15af16fb2355c23c7269d8f
bencoscia <benjamin.coscia@colorado.edu>
2018-11-13 15:57:27 -0700

    New solutes!

--- setup/param.sh
+++ setup/param.sh
@@ -13,7 +13,7 @@ name='monomer' # name of molecule. This name will carry through to output
 nc=0  # net charge
 res='MON' # name of residue being made, NOTE: This must match the residue in the .pdb file or you will get errors
 anneal='no'  # change to 'yes' if you want a thermal annealing process carried out after energy minimization
-input_path="${SCRIPT_DIR}/../Parameterization/"  # location of em.mdp and anneal.mdp files
+input_path="${SCRIPT_DIR}/../Paramaterization"  # location of em.mdp and anneal.mdp files
 
 while getopts "n:c:r:a:p:" opt; do
     case $opt in
@@ -28,7 +28,7 @@ done
 # atomtyping and initial charge assigment with antechamber and tleap roughly follow steps descrbed here:
 # http://ambermd.org/tutorials/basic/tutorial4b/
 
-#antechamber -i ${name}.pdb -fi pdb -o ${name}.mol2 -fo mol2 -c bcc -s 2 -nc ${nc}  # The .pdb must have connectivity info!
+antechamber -i ${name}.pdb -fi pdb -o ${name}.mol2 -fo mol2 -c bcc -s 2 -nc ${nc}  # The .pdb must have connectivity info!
 # -c bcc tells antechamber to use AM1-BCC charge model
 # -s flag just defines verbosity of output
 parmchk -i ${name}.mol2 -f mol2 -o ${name}.frcmod
