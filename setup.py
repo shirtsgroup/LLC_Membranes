fa11bcd1313a9342c4fb18283c64a8ce8e70cf0d
bencoscia <benjamin.coscia@colorado.edu>
2019-02-21 12:47:15 -0700

    Trying to get cython to pip install first

--- setup.py
+++ setup.py
@@ -12,10 +12,9 @@ setup(
     # Self-descriptive entries which should always be present
     name='LLC_Membranes',
     version = 0.1,
-    description = 'Setup, simulate and analyze MD simulations of lyotropic liquid crystal membranes',
+    description = 'Set up, simulate and analyze MD simulations of lyotropic liquid crystal membranes',
     author='Ben Coscia',
     license='BSD-3-Clause',
     install_requires=['numpy', 'mdtraj', 'matplotlib', 'ruptures', 'scipy', 'tqdm', 'fbm'], 
-
     packages = ['LLC_Membranes', 'LLC_Membranes.setup', 'LLC_Membranes.analysis', 'LLC_Membranes.llclib', 'LLC_Membranes.timeseries'],
 )
