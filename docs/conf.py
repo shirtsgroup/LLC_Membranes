94d13ad33724ae49163b18bb9fbc497ea74d088d
bencoscia <benjamin.coscia@colorado.edu>
2019-02-21 15:09:45 -0700

    added pandas and trying to make it find LLC_Membranes

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ import os
 import sys
 from unittest.mock import MagicMock
 
+sys.path.insert(0, os.path.abspath('../'))
 sys.path.insert(0, os.path.abspath('../LLC_Membranes'))
 sys.path.insert(0, os.path.abspath('../LLC_Membranes/setup'))
 sys.path.insert(0, os.path.abspath('../LLC_Membranes/analysis'))
